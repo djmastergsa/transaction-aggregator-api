639f0584d5dd826252db490e33f69fa6aefd95f7
Master G <djmastergsa@gmail.com>
2026-04-06 15:38:30 +0200

    Bootstrap Spring Boot project with dependencies and configuration

---
+++ src/main/java/com/capitec/aggregator/TransactionAggregatorApplication.java
@@ -0,0 +1,12 @@
+package com.capitec.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionAggregatorApplication.class, args);
+    }
+}
